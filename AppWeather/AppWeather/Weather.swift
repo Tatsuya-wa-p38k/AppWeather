c5d9d690fc160fd2be396b6291eba31477433638
spark-06 <>
2024-05-28 16:40:56 +0900

    07.途中まで　エラー一つある

--- AppWeather/AppWeather/Weather.swift
+++ AppWeather/AppWeather/Weather.swift
@@ -8,31 +8,50 @@
 import Foundation
 import YumemiWeather
 
+struct Date:Codable {
+    let area:String
+    let date:String
+}
+
+struct Weather:Codable {
+    let maxTemperature: Int
+    let minTemperature: Int
+    let setWeatherType: String
+
+    enum CodingKeys: String, CodingKey {
+        case maxTemperature = "max_temperature"
+        case minTemperature = "min_temperature"
+        case setWeatherType = "weather_condition"
+    }
+
+}
+
+
 protocol WeatherDelegate {
-    func setWeatherType(type: String)
     //エラー発生時の処理をプロトコル記載する
     func didEncounterError(error: Error)
 
-    func maxTemperature(max:Int)
-    func minTemperature(min:Int)
+    //新しい関数setWeatherを作り、structで３つのデータをまとめた
+    func setWeather(weather: Weather)
 }
 
 class WeatherDetail {
     
     var delegate:WeatherDelegate?
 
-    let sendJsonString = """
-{
-    "area": "Tokyo",
-    "date": "2020-04-01T12:00:00+09:00"
-}
-"""
-
     func setWeatherType() {
-
+        let sendJsonString = Date(area: "Tokyo",
+                                  date: "2020-04-01T12:00:00+09:00")
         //エラー発生時の処理をdelegateを用いて記載する
         do {
+            let encoder = JSONEncoder()//エンコーダーの準備
+            let jsonData = try encoder.encode(sendJsonString)
+            guard let sendJsonString = String(data:jsonData, encoding: .utf8) else {
+                return
+            }
+
             let fetchWeatherString = try YumemiWeather.fetchWeather(sendJsonString)
+
 
             // 1. UTF-8エンコーディングでDataオブジェクトに変換
             guard let jsonData = fetchWeatherString.data(using: .utf8),
@@ -45,9 +64,10 @@ class WeatherDetail {
             else {
                 return
             }
-            self.delegate?.setWeatherType(type: weatherCondition)
-            self.delegate?.maxTemperature(max: maxTemperature)
-            self.delegate?.minTemperature(min: minTemperature)
+
+            let weather = try JSONDecoder().decode(Weather.self, from: jsonData)
+            //新しい関数setWeatherを作り、structで３つのデータをまとめた
+            self.delegate?.setWeather(weather: Weather)
 
 
         } catch {
